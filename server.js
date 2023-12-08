8882182e4a3564294ab33b24a1a45d41d38c87ef
KAITLIN DAVIS <KAITLIN.A.DAVIS1996@GMAIL.COM>
2023-12-08 00:25:40 -0600

    updated weather

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 
+let weatherData;
+
+try {
+  weatherData = require('./data/weather.json');
+  console.log(weatherData); 
+} catch (error) {
+  console.error('Error reading the weather data:', error);
+}
+
 app.get('/', (req, res) => {
   res.send('City Explorer');
+});
+
+app.get('/weather', (req, res) => {
+  res.json(weatherData);
 });
 
 const PORT = process.env.PORT || 3000;
